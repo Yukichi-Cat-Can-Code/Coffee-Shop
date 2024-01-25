d4f78c2b952205b3bdc7735d1c4d948647ccf6ef
Rafiqul Islam <rafiquljakir@gmail.com>
2024-01-25 11:14:06 +0600

    User registration done

--- auth/register.php
+++ auth/register.php
@@ -1,10 +1,24 @@
 <?php
 	require "../includes/header.php";
+  require "../config/config.php";
 ?>
 <?php
   if(isset($_POST['submit'])){
-    if(empty($_POST['user_name']) || empty($_POST['password']) || empty($_POST['email']) ){
+    if(empty($_POST['user_name']) || empty($_POST['user_email']) || empty($_POST['user_pass']) ){
+      echo "<script> alert('one or more infield are empty') </script>";
+    }else{
+      $user_name = $_POST['user_name'];
+      $user_email = $_POST['user_email'];
+      $user_pass = password_hash($_POST['user_pass'], PASSWORD_DEFAULT);
+      
+      $insert = $conn->prepare("INSERT INTO users(user_name, user_email,user_pass) VALUES(:user_name, :user_email, :user_pass)");
+      $insert->execute([
+        ":user_name" => $user_name,
+        ":user_email" => $user_email,
+        ":user_pass" => $user_pass,
+      ]);
 
+      header("location: login.php");
     }
   }
 
@@ -31,34 +45,34 @@
       <div class="container">
         <div class="row">
           <div class="col-md-12 ftco-animate">
-			<form action="#" class="billing-form ftco-bg-dark p-3 p-md-5">
+			<form action="register.php" method = "POST" class="billing-form ftco-bg-dark p-3 p-md-5">
 				<h3 class="mb-4 billing-heading">Register</h3>
 	          	<div class="row align-items-end">
                  <div class="col-md-12">
                         <div class="form-group">
                             <label for="Username">Username</label>
-                          <input type="text" class="form-control" placeholder="Username">
+                          <input type="text" name="user_name" class="form-control" placeholder="Username">
                         </div>
                  </div>
 	          	  <div class="col-md-12">
 	                <div class="form-group">
 	                	<label for="Email">Email</label>
-	                  <input type="text" class="form-control" placeholder="Email">
+	                  <input type="text" name="user_email" class="form-control" placeholder="Email">
 	                </div>
 	              </div>
                  
 	              <div class="col-md-12">
 	                <div class="form-group">
 	                	<label for="Password">Password</label>
-	                    <input type="password" class="form-control" placeholder="Password">
+	                    <input type="password" name="user_pass" class="form-control" placeholder="Password">
 	                </div>
 
                 </div>
                 <div class="col-md-12">
                 	<div class="form-group mt-4">
 							<div class="radio">
-                                <button class="btn btn-primary py-3 px-4">Register</button>
-						    </div>
+                <button class="btn btn-primary py-3 px-4" name="submit">Register</button>
+						  </div>
 					</div>
                 </div>
 
--- config/config.php
+++ config/config.php
@@ -1,18 +1,12 @@
 <?php
 
     try{
-        //Host
-        define("HOST", "localhost");
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "coffee-shop";
 
-        //dbName
-        define("DBNAME", "coffee-shop");
-
-        //user
-        define("USER", "root");
-
-        //password
-        define("PASS", "");
-        $conn = new PDO("mysql:host = ".HOST.";dbname = ".DBNAME."",USER,PASS);
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     }catch(PDOException $Exception){
