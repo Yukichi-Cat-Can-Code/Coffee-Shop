573e11a8aba0d1c4906e81ddc0ee7f8ec3709047
Rafiqul Islam <rafiquljakir@gmail.com>
2024-01-31 23:54:58 +0600

    Working With Admin Index Page

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -1,8 +1,26 @@
 <?php 
   require "layouts/header.php" ;
+  require "../config/config.php" ;
+
   if(!isset($_SESSION['admin_name'])){
     header("location: http://localhost/coffee-Shop/admin-panel/admins/login-admins.php");
   }
+
+  //query for Product
+  $products = $conn->query("SELECT * from product");
+  $products->execute();
+
+  //query for orders;
+  $orders = $conn->query("SELECT * FROM orders");
+  $orders->execute();
+
+  //query for Bookings
+  $bookings = $conn->query("SELECT * FROM bookings");
+  $bookings->execute();
+
+  //query for Admins
+  $admin = $conn->query("SELECT * FROM admins");
+  $admin->execute();
 ?>
     <div class="container-fluid">      
       <div class="row">
@@ -10,8 +28,7 @@
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">Products</h5>
-              <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
-              <p class="card-text">number of products: 8</p>
+              <p class="card-text">number of products: <?php echo $products->rowcount(); ?></p>
              
             </div>
           </div>
@@ -21,7 +38,7 @@
             <div class="card-body">
               <h5 class="card-title">Orders</h5>
               
-              <p class="card-text">number of orders: 4</p>
+              <p class="card-text">number of orders: <?php echo $orders->rowcount(); ?></p>
               
             </div>
           </div>
@@ -31,7 +48,7 @@
             <div class="card-body">
               <h5 class="card-title">Bookings</h5>
               
-              <p class="card-text">number of bookings: 4</p>
+              <p class="card-text">number of bookings: <?php echo $bookings->rowcount(); ?></p>
               
             </div>
           </div>
@@ -41,7 +58,7 @@
             <div class="card-body">
               <h5 class="card-title">Admins</h5>
               
-              <p class="card-text">number of admins: 3</p>
+              <p class="card-text">number of admins: <?php echo $admin->rowcount(); ?></p>
               
             </div>
           </div>
